778331ad330c9d8513a577481c962a1a0f559dc9
Hossein806 <hghaffari806@gmail.com>
2024-09-25 16:35:10 +0330

    Release 1.1.0-2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.borna.dotinfilm"
         minSdk = ProjectConfig.minSdk
         targetSdk = ProjectConfig.targetSdk
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
